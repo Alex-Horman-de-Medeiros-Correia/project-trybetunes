32769a4c449038714d7481cc9bca6fa241616b92
Alex-Horman-de-Medeiros-Correia <alexhorman1990@gmail.com>
2022-04-03 01:03:40 -0300

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,50 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artista: '',
+      botaoRender: true,
+    };
+  }
+
+  renderMin = ({ target: { value } }) => {
+    const minDigite = 2;
+    const digitado = value.length;
+
+    this.setState({
+      artista: value,
+      botaoRender: digitado < minDigite,
+    });
+  }
+
   render() {
+    const { botaoRender, artista } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
+
+        <form>
+          <input
+            type="text"
+            name="artista"
+            value={ artista }
+            data-testid="search-artist-input"
+            onChange={ (event) => this.renderMin(event) }
+          />
+          <br />
+          <br />
+          <button
+            type="button"
+            disabled={ botaoRender }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
