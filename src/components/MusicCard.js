cfee4f0d57b68d390d53173a89a3c27c69d376e3
Alex-Horman-de-Medeiros-Correia <alexhorman1990@gmail.com>
2022-04-03 05:07:24 -0300

    requisito 7

---
+++ src/components/MusicCard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+class MusicCard extends React.Component {
+  render() {
+    const { trackName, previewUrl } = this.props;
+
+    return (
+      <div>
+        <h1>
+          {' '}
+          { `${trackName}` }
+          {' '}
+        </h1>
+
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          <code>audio</code>
+        </audio>
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  trackName: propTypes.string.isRequired,
+  previewUrl: propTypes.string.isRequired,
+};
+
+export default MusicCard;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,14 +1,74 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artista: '',
+      album: '',
+      musicas: [],
+    };
+  }
+
+  componentDidMount() {
+    this.allMusics();
+  }
+
+  allMusics = async () => {
+    const { match: { params: { id } } } = this.props;
+
+    const requerimento = await getMusics(id);
+    const filterMusics = requerimento.filter((element) => element.kind);
+    // A respeito desse filter, peguei dicas com um Dev mais experiente
+
+    this.setState({
+      artista: requerimento[0].artistName,
+      album: requerimento[0].collectionName,
+      musicas: [...filterMusics],
+    });
+  }
+
   render() {
+    const { artista, album, musicas } = this.state;
+
     return (
       <div data-testid="page-album">
         <Header />
+
+        <h1 data-testid="artist-name">
+          {' '}
+          { `${artista}` }
+          {' '}
+        </h1>
+        <h2 data-testid="album-name">
+          {' '}
+          { `${album}` }
+          {' '}
+        </h2>
+
+        { musicas.map((element) => (
+          <MusicCard
+            key={ element.trackId }
+            trackName={ element.trackName }
+            previewUrl={ element.previewUrl }
+          />
+        ))}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default Album;
